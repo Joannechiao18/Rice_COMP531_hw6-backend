9f7e687b0093471aac9f2ebb3ef424026cca49f9
Joanne Wang <84509949+Joannechiao18@users.noreply.github.com>
2023-11-14 17:14:12 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,16 +1,36 @@
 const mongoose = require("mongoose");
-const connectionString = process.env.DB_CONNECTION_STRING;
+
+// Using the MONGODB_URI environment variable for the MongoDB connection string
+const mongoDBUri = process.env.MONGODB_URI;
 
 mongoose
-  .connect(connectionString, {
+  .connect(mongoDBUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .catch((error) => {
-    console.log({ error });
+    // Log the error if the connection fails
+    console.error("MongoDB connection error:", error);
   });
 
-// Connection successful
+// Event listener for successful connection
 mongoose.connection.on("connected", () => {
-  console.log("Mongoose connection open to " + 11);
+  console.log("Mongoose connection successfully opened to MongoDB");
+});
+
+// Optional: Add event listeners for other connection events for better monitoring
+mongoose.connection.on("error", (err) => {
+  console.error("Mongoose connection error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose connection disconnected");
+});
+
+// Optional: Handle Node.js process termination to close the Mongoose connection
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log("Mongoose connection closed due to application termination");
+    process.exit(0);
+  });
 });
